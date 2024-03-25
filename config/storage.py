286d5ab254cbe3047e77755ec5083d916e45bc71
Vukovic Nikola <admin@nikolav.rs>
2024-03-25 22:45:53 +0100

    Mon Mar 25 21:45:53 UTC 2024

--- config/storage.py
+++ config/storage.py
@@ -1,3 +1,8 @@
 
-ALLOWED_EXTENSIONS = {'*', 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'md'}
+# @storage: --allow-all
+# ALLOWED_EXTENSIONS = {'*', 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'md'}
+
+# @storage: --allow-images
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 RE_EXT = r'^.*\.([^\.]+)$'
