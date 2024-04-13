1934b7c8eef00a176bce9c1389e2dd853ea3d981
Vukovic Nikola <admin@nikolav.rs>
2024-04-13 08:41:15 +0200

    ignore /demo.py

--- config/storage.py
+++ config/storage.py
@@ -1,8 +1,8 @@
 
-# @storage: --allow-all
+# @storage:allow-all
 # ALLOWED_EXTENSIONS = {'*', 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'md'}
 
-# @storage: --allow-images
+# @storage:allow-images
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 RE_EXT = r'^.*\.([^\.]+)$'
