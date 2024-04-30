580cef122f50a4185be9de60e5fea5d173d01d60
Vukovic Nikola <admin@nikolav.rs>
2024-04-30 22:06:25 +0200

    Tue Apr 30 22:06:25 CEDT 2024

--- config/graphql/resolvers/query/products/products_list_all.py
+++ config/graphql/resolvers/query/products/products_list_all.py
@@ -1,6 +1,10 @@
+from sqlalchemy.orm import joinedload
 from flask_app import db
 
+from models.tags import Tags
+from models.users import Users
 from models.products import Products
+from models.docs import Docs
 
 from config.graphql.init import query
 
@@ -13,10 +17,15 @@ def resolve_productsListAll(_obj, _info):
   try:
     products = db.session.scalars(
       db.select(Products)
-    )
+        .options(
+          joinedload(Products.tags), 
+          joinedload(Products.user), 
+          joinedload(Products.docs)
+        )
+    ).unique()
     return SchemaSerializeProductsTimes(many = True).dump(products)
 
-  except:
-    pass
+  except Exception as err:
+    raise err
 
   return []
